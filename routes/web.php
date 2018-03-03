317d31fafeeadd7fb86044b69242d34d8902270a
DJDLee <l353861350@163.com>
2018-03-03 17:21:02 +0000

    Add password resets & email configuration

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,19 @@ Route::get('about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
 
 Route::resource('users', 'UsersController');
-
+//显示账户登录界面
 Route::get('login', 'SessionsController@create')->name('login');
-
+//账户登录操作
 Route::post('login', 'SessionsController@store')->name('login');
-
+//账户退出操作
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
-
+//激活账户操作
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+//显示重置密码邮件发送页面
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+//发送重置密码链接
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+//显示重置密码设置页面
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+//更新密码操作
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
